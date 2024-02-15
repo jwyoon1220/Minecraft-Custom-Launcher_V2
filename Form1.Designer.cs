2691979e9a731a3672d867cf4edfcb9eff3dbfa3
jwyoon1220 <jwyoon1220@naver.com>
2024-02-15 14:28:59 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Minecraft_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(765, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "---------------------------------------------------------------------------------" +
+    "---------------------------------------------";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(360, 369);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(133, 63);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "마인크래프트 실행하기";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 79);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(157, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "나중에 예쁘게 꾸밀거임";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(23, 177);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(87, 19);
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "총 파일 수: 0";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(23, 120);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(72, 19);
+            this.metroLabel4.TabIndex = 6;
+            this.metroLabel4.Text = "파일종류: ";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(23, 139);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(72, 19);
+            this.metroLabel5.TabIndex = 7;
+            this.metroLabel5.Text = "파일이름: ";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(23, 158);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(115, 19);
+            this.metroLabel6.TabIndex = 8;
+            this.metroLabel6.Text = "진행한 파일 수: 0";
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Location = new System.Drawing.Point(14, 403);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(216, 29);
+            this.metroComboBox1.TabIndex = 16;
+            this.metroComboBox1.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "마크?런처";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using MetroFramework.Forms;
+using CmlLib.Core;
+using System;
+using CmlLib.Core.Auth.Microsoft;
+using CmlLib.Core.Installer.FabricMC;
+using System.Runtime.CompilerServices;
+using CmlLib.Core.Version;
+using System.Windows.Forms;
+using CmlLib.Core.Auth;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Minecraft_Launcher
+{
+    public partial class Form1 : MetroForm
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var path = new MinecraftPath();
+            var launcher = new CMLauncher(path);
+            var versions = await launcher.GetAllVersionsAsync();
+            var fabricVersionLoader = new FabricVersionLoader();
+            var fabricVersions = await fabricVersionLoader.GetVersionMetadatasAsync();
+            metroComboBox1.Items.Clear();
+            foreach (var v in versions)
+            {
+                metroComboBox1.Items.Add(v.Name);
+            }
+            foreach (var v in fabricVersions)
+            {
+                metroComboBox1.Items.Add(v.Name);
+            }
+        }
+
+     
+
+        private async void metroButton2_Click(object sender, EventArgs ev)
+        {
+            
+            Console.WriteLine("다운로드 시작");
+            System.Net.ServicePointManager.DefaultConnectionLimit = 256;
+
+
+            var path = new MinecraftPath();
+            var launcher = new CMLauncher(path);
+
+            
+            launcher.FileChanged += (e) =>
+            {
+                metroLabel4.Text = ("파일종류: " + e.FileKind.ToString());
+                metroLabel5.Text = ("파일이름: " + e.FileName);
+                metroLabel6.Text = ("진행한 파일 수: " + e.ProgressedFileCount + " / " + e.TotalFileCount);
+                metroLabel3.Text = ("총 파일 수: " + e.TotalFileCount);
+                
+            };
+
+            var loginHandler = JELoginHandlerBuilder.BuildDefault();
+            Console.WriteLine("login...");
+
+            try
+            {
+                var session = await loginHandler.Authenticate();
+                Console.WriteLine("로그인 성공");
+                var fabricVersionLoader = new FabricVersionLoader();
+
+                var fabricVersions = await fabricVersionLoader.GetVersionMetadatasAsync();
+
+                var versionName = metroComboBox1.SelectedItem.ToString();
+
+                var fabric = fabricVersions.GetVersionMetadata(versionName);
+                await fabric.SaveAsync(path);
+
+                var process = await launcher.CreateProcessAsync(versionName, new MLaunchOption
+                {
+                    MaximumRamMb = 4096,
+                    Session = session
+                });
+
+                process.Start();
+                Console.WriteLine("다운로드 완료");
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("마인크래프트 실행 실패\n", "마인크래프트", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+    
+       
+    }
+}
